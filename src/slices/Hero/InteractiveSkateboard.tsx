62eb226912abfa5937fc7d85df3cf81c7f8cf177
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-03-24 15:55:20 +0100

    Ajout de l'animation gsap pour le ollie

--- src/slices/Hero/InteractiveSkateboard.tsx
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import { Skateboard } from '@/components/Skateboard'
-import { ContactShadows, Environment, OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React, { Suspense } from 'react'
+import * as THREE from "three";
+import { Skateboard } from '@/components/Skateboard';
+import { ContactShadows, Environment, OrbitControls } from '@react-three/drei';
+import { Canvas, ThreeEvent } from '@react-three/fiber';
+import React, { Suspense, useRef } from 'react';
+import gsap from "gsap";
 
 type Props = {
     deckTextureURL: string;
@@ -35,19 +37,48 @@ function Scene(
         deckTextureURL, wheelTextureURL, truckColor, boltColor
     }: Props
 ) {
+    const containerRef = useRef<THREE.Group>(null);
+
+    function onClick(event: ThreeEvent<MouseEvent>){
+        event.stopPropagation()
+
+        const board = containerRef.current;
+
+        if(!board) return
+
+        const {name} = event.object;
+
+        gsap.timeline()
+        .to(board.position,{ y: .8, duration : .51, ease: "power.out", delay: .26})
+        .to(board.position, { y:0, duration: 0.43, ease: "power2.in"}) ;
+
+        gsap.timeline()
+        .to(board.rotation,{ x: -.6, duration: .26, ease: "none"})
+        .to(board.rotation,{ x: .4, duration: .82, ease: "power2.in"})
+        .to(board.rotation,{ x: 0, duration: .12, ease: "none"})
+    }
+
     return (
         <group>
             <OrbitControls />
             <Environment files={"/hdr/warehouse-256.hdr"} />
-            <Skateboard
-                wheelTextureURLs={[wheelTextureURL]}
-                wheelTextureURL={wheelTextureURL}
-                deckTextureURLs={[deckTextureURL]}
-                deckTextureURL={deckTextureURL}
-                truckColor={truckColor}
-                boltColor={boltColor}
-                constantWheelSpin
-            />
+            <group ref={containerRef}>
+
+                <Skateboard
+                    wheelTextureURLs={[wheelTextureURL]}
+                    wheelTextureURL={wheelTextureURL}
+                    deckTextureURLs={[deckTextureURL]}
+                    deckTextureURL={deckTextureURL}
+                    truckColor={truckColor}
+                    boltColor={boltColor}
+                    constantWheelSpin
+                />
+
+                <mesh position={[0, .27, 0]} name="middle" onClick={onClick}>
+                    <boxGeometry args={[.6, .1, 2.2]}/>
+                    <meshStandardMaterial visible={true}/>
+                </mesh>
+            </group>
             <ContactShadows opacity={0.6} position={[0, -.08, 0]} />
         </group>
     )
