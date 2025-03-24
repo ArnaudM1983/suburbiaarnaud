6f03d1704784acaf99fa1febe7edf48c5217c0d3
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-03-24 17:49:40 +0100

    Ajout des points cliquables pour déclencher les tricks

--- src/slices/Hero/InteractiveSkateboard.tsx
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -4,9 +4,10 @@ import * as THREE from "three";
 import { Skateboard } from '@/components/Skateboard';
 import { ContactShadows, Environment, OrbitControls } from '@react-three/drei';
 import { Canvas, ThreeEvent } from '@react-three/fiber';
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense, useRef, useState } from 'react';
 import gsap from "gsap";
 import { isKeyObject } from "util/types";
+import { Hotspot } from "./Hotspot";
 
 type Props = {
     deckTextureURL: string;
@@ -36,10 +37,17 @@ export function InteractiveSkateboard({
 function Scene(
     {
         deckTextureURL, wheelTextureURL, truckColor, boltColor
-    }: Props
-) {
+    }: Props) {
     const containerRef = useRef<THREE.Group>(null);
     const originRef = useRef<THREE.Group>(null);
+
+    const [animating, setAnimating] = useState(false)
+
+    const [showHotspot, setShowHotspot] = useState({
+        front: true,
+        middle: true,
+        back: true,
+    });
 
     function onClick(event: ThreeEvent<MouseEvent>) {
         event.stopPropagation()
@@ -47,10 +55,12 @@ function Scene(
         const board = containerRef.current;
         const origin = originRef.current
 
-        if (!board || !origin) return
+        if (!board || !origin || animating) return
 
         const { name } = event.object;
- 
+
+        setShowHotspot((current) => ({ ...current, [name]: false }))
+
         if (name === "back") {
             ollie(board);
         } else if (name === "middle") {
@@ -94,7 +104,8 @@ function Scene(
 
     // Jump Commun 
     function jumpBoard(board: THREE.Group) {
-        gsap.timeline()
+        setAnimating(true);
+        gsap.timeline({onComplete: ()=> setAnimating(false)})
             .to(board.position, { y: .8, duration: .51, ease: "power.out", delay: .26 })
             .to(board.position, { y: 0, duration: 0.43, ease: "power2.in" });
     }
@@ -115,14 +126,17 @@ function Scene(
                             boltColor={boltColor}
                             constantWheelSpin
                         />
+                        <Hotspot isVisible={!animating && showHotspot.front} position={[0, .38, 1]} color="#B8FC39" />
                         <mesh position={[0, .27, 0.9]} name="front" onClick={onClick}>
                             <boxGeometry args={[.6, .2, 0.58]} />
                             <meshStandardMaterial visible={false} />
                         </mesh>
+                        <Hotspot isVisible={!animating && showHotspot.middle} position={[0, .33, 0]} color="#FF7A51" />
                         <mesh position={[0, .27, 0]} name="middle" onClick={onClick}>
                             <boxGeometry args={[.6, .1, 1.2]} />
                             <meshStandardMaterial visible={false} />
                         </mesh>
+                        <Hotspot isVisible={!animating && showHotspot.back} position={[0, .35, -0.9]} color="#46ACFA" />
                         <mesh position={[0, .27, -0.9]} name="back" onClick={onClick}>
                             <boxGeometry args={[.6, .2, 0.58]} />
                             <meshStandardMaterial visible={false} />
