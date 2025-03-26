9b0a11f72a695dde51fd577e15af2e95aa953edf
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-03-26 18:42:09 +0100

    Maj Buttonlink.tsx

--- src/components/buttonLink.tsx
+++ src/components/buttonLink.tsx
@@ -19,16 +19,16 @@ export function ButtonLink({
   return (
     <PrismicNextLink
       className={clsx(
-        "button-cutout group inline-flex items-center bg-gradient-to-b from-25% to-75% bg-[length:100%_400%] font-bold transition-[filter,background-position] duration-300 hover:bg-bottom",
+        "button-cutout group mx-4 inline-flex items-center bg-gradient-to-b from-25% to-75% bg-[length:100%_400%] font-bold transition-[filter,background-position] duration-300 hover:bg-bottom",
         size === "sm" && "gap-2.5 py-2 text-base",
         size === "md" && "gap-3 px-1 text-lg ~py-2.5/3",
-        size === "lg" && "~text-lg/2xl gap-3 px-6 py-3",
+        size === "lg" && "~text-lg/2xl ~gap-3/4 ~px-1/2 ~py-3/4",
         color === "orange" &&
           "from-brand-orange to-brand-lime text-black hover:text-black",
         color === "purple" &&
           "from-brand-purple to-brand-lime text-white hover:text-black",
         color === "lime" && "from-brand-lime to-brand-orange text-black",
-        className,
+        className
       )}
       {...props}
     >
@@ -39,7 +39,7 @@ export function ButtonLink({
               "flex size-6 items-center justify-center transition-transform group-hover:-rotate-[25deg] [&>svg]:h-full [&>svg]:w-full",
               size === "sm" && "size-5",
               size === "md" && "size-6",
-              size === "lg" && "~size-6/8",
+              size === "lg" && "~size-6/8"
             )}
           >
             {icon === "cart" && <FaCartShopping />}
